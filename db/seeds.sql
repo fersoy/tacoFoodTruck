10e91e7680a5b44470ca5c3fcc81bff0ccf0d320
Fatih Ersoy <f.ersoytx@gmail.com>
2019-06-02 04:47:45 -0500

    some codes added to the files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO tacos (taco_name, shell, vegetarian, picked_up) VALUES ('cheese', 'soft~flour', true, true);
+INSERT INTO tacos (taco_name, shell, vegetarian, picked_up) VALUES ('beef', 'hard~corn', false, true);
+INSERT INTO tacos (taco_name, shell, vegetarian, picked_up) VALUES ('cheese and beef', 'soft~flour', false, true);
