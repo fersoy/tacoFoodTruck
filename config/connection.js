81fb3f9c60788d9e29d03d1cfa39bdcc7447e78b
Fatih Ersoy <f.ersoytx@gmail.com>
2019-06-05 23:27:27 -0500

    initial commit

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "tacos_db",
-    socket: "/Applications/MAMP/tmp/mysql/mysql.sock"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+
+        host: "localhost",
+        port: 8889,
+        user: "root",
+        password: "root",
+        database: "tacos_db",
+        socket: "/Applications/MAMP/tmp/mysql/mysql.sock"
+    });
+};
 
 // Make connection.
 connection.connect(function (err) {
